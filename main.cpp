1c35d52fa02b075b0a3dc60ba10a98bcda7e504f
guilhermecpp <guilhermecdemenezes123@gmail.com>
2020-05-06 10:05:50 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    char sexo;
+    int qtd_cer;
+    int qtd_ref;
+    int qtd_esp;
+
+    printf("Sexo: ");
+    scanf("%c", &sexo);
+
+    printf("Quantidade de cervejas: ");
+    scanf("%d", &qtd_cer);
+
+    printf("Quantidade de refrigerantes: ");
+    scanf("%d", &qtd_ref);
+
+    printf("Quantidade de espetinhos: ");
+    scanf("%d", &qtd_esp);
+
     return 0;
 }
